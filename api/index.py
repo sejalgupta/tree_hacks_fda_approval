ea9ce7ec506e8b1f360ffba560ab39ff407e9b68
sejalgupta4902 <sejalg@mit.edu>
2024-02-16 21:09:48 -0800

    initial commit to fix dependencies

--- api/index.py
+++ api/index.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template_string
 app = Flask(__name__)
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/api/python", methods=['GET'])
+def submit_form():
+    form_html = '''
+    <form action="/api/handle-form" method="post">
+        <label for="device-description">Device Description:</label><br>
+        <input type="text" id="device-description" name="device-description" required><br>
+        <label for="indication-for-use">Indication for Use:</label><br>
+        <input type="text" id="indication-for-use" name="indication-for-use" required><br><br>
+        <input type="submit" value="Submit">
+    </form> 
+    '''
+    return render_template_string(form_html)
