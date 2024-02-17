d0a5ead1e0d2aa2960cbf6caa846725e34157f76
natashamaniarnew <114311370+natashamaniarnew@users.noreply.github.com>
2024-02-16 23:11:02 -0800

    add_pages

---
+++ pages/userinput.js
@@ -0,0 +1,10 @@
+import DeviceForm from '../components/DeviceForm';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className="text-center text-2xl font-bold my-6">Device Information Form</h1>
+      <DeviceForm />
+    </div>
+  );
+}
